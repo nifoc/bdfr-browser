5d97a4f1d3ed9e5383b1527425d0ea012df4c505
Daniel Kempkens <daniel+git@kempkens.io>
2023-08-15 13:56:44 +0200

    chore: Move comment rendering to helper

--- lib/bdfr_browser/render_utils.ex
+++ lib/bdfr_browser/render_utils.ex
@@ -1,4 +1,8 @@
 defmodule BdfrBrowser.RenderUtils do
+  def comment(cmt) do
+    Earmark.as_html!(cmt)
+  end
+
   def message(msg) do
     img_replacement = "<img src=\"/chat_media/\\1\" style=\"max-width: 500px;\" alt=\"Image\" />"
 
